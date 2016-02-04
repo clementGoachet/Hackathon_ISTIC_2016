2d8264cc9b55aaa2faf180d512575287f9a9af99
florentL <florent.le.b@hotmail.fr>
2016-02-04 18:13:24 +0100

    Added homepage

--- Js/main.js
+++ Js/main.js
@@ -10,17 +10,34 @@
 		
 );
 
-$('form[name="search-imdb"]').on("submit", function(e) {
-    var form = $(this);
-    var query = $("#input_search").val();
-    e.preventDefault();
-    $.ajax({
-        url: "http://imdb.wemakesites.net/api/search?q="+query,
-        data: form.serialize(),
-        crossDomain: true,
-        dataType: "jsonp",
-        success: function(data) {
-            window.console.log(data);
-        }
-    });
+$(document).ready(function() {
+
+	$('form[name="search-imdb"]').on("submit", function(e) {
+	    var form = $(this);
+	    var query = $("#input_search").val();
+	    e.preventDefault();
+	    $.ajax({
+	        url: "http://imdb.wemakesites.net/api/search?q="+query,
+	        data: form.serialize(),
+	        crossDomain: true,
+	        dataType: "jsonp",
+	        success: function(data) {
+	            window.console.log(data);
+	        }
+	    });
+	});
+	
+	$(".dropdown").change(function() {
+		var val = $(this).val();
+		val = val.toLowerCase();
+		$(".input_container input").attr("placeholder", "Entrez un "+val);
+	});
+	
+	$('#slides').superslides(
+			{'animation': 'fade',
+			'animation_speed': 3500
+			}
+	);
+	
+	$(window).trigger('resize').trigger('scroll'); /* refresh sizing */
 });
