a1a95cab46b411c155e84b4eca9c3d145a482851
pchmn <paul.chemin35@gmail.com>
2016-02-04 16:27:33 +0100

    test graph

--- Js/vis_tools.js
+++ Js/vis_tools.js
@@ -14,8 +14,59 @@ function getFilmInfos(json) {
     return result;
 }
 
-function createGrpah(json) {
+function createGraphFromFilm(idFilm) {
+    // id en mode numérique pour les nodes
+    var idFilmNum = Number(idFilm.replace( /^\D+/g, ''));
+    var nodes_data = [];
+    var edges_data = [];
 
+    $.ajax({
+        type: "get",
+        url: "../json/films_test.json",
+        datatype: "json",
+        success: function (data) {
+            console.log(data.data);
+        },
+       //add this error handler you'll get alert
+        error: function (request, status, error) {
+            alert(request.responseText);
+        }
+    });
+    $.getJSON("../json/films_test.json", function (data) {
+        console.log("in")
+    });
+
+/*                    nodes_data.push({
+                        id: idFilmNum,
+                        group: 'films',
+                        label: film.Title,
+                        shape: 'circularImage',
+                        image: film.Poster
+                    });
+
+                    console.log(nodes_data)
+
+                    // noeuds des acteurs
+                    $.each(film.Actors, function(key, acteur) {
+                        var idActeurNum = Number(acteur.replace( /^\D+/g, ''));
+
+                        nodes_data.push({
+                            id: idActeurNum,
+                        });
+
+                        edges_data.push({
+                            from: idFilmNum,
+                            to: idActeurNum
+                        });
+                    });*/
+
+    var nodes = new vis.DataSet(nodes_data);
+    var edges = new vis.DataSet(edges_data);
+
+    return {
+        nodes: nodes,
+        edges: edges
+    }
 }
 
 function createFilmInfosHtml(json) {
